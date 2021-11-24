64c1777bb4f15724ed783d16db32f11064b4f951
Diaz Ignacio 79861 <ignacio_dior@hotmail.com>
2021-11-24 03:33:21 -0300

    Prueba3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>Chipu the machine</code> and save to reload.
+          Edit <code>Chipu the machine</code> holabolo.
         </p>
         <a
           className="App-link"
