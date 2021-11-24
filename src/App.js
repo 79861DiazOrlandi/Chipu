ea2f53e7431900d8e0a510c12dbee0dfbe4c80fa
Diaz Ignacio 79861 <ignacio_dior@hotmail.com>
2021-11-24 04:53:55 -0300

    asfasf

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>Chipu the machine</code> hola.
+          Edit <code>Chipu the machine</code> hola1.
         </p>
         <a
           className="App-link"
