29df9a12b3d63269405321e6c71936186bb2ef1f
Diaz Ignacio 79861 <ignacio_dior@hotmail.com>
2021-11-24 05:02:28 -0300

    que onda

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>Chipu the machine</code> hola1.
+          Edit <code>Chipu the machine</code> holaholahola.
         </p>
         <a
           className="App-link"
